7b52d1f5adcd01eb9bb554e454fc8c5419b3fb6e
Peter Blanchfield <zygomy@gmail.com>
2023-01-13 12:33:34 +0000

    added a unity project
    so it has something to push

---
+++ MyNothingProject/Assets/Starter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Starter : MonoBehaviour
+{
+    public Text textMessage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMessage.text = "Give me help";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
